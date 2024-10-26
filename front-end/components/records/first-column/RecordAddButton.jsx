4d86174872a2ebbbc2f237debfcc84be566d53d3
Tuugii <namjildorjtuguldur1234@gmail.com>
2024-10-26 23:55:07 +0800

    lets try be aware of yourself

--- front-end/components/records/first-column/RecordAddButton.jsx
+++ front-end/components/records/first-column/RecordAddButton.jsx
@@ -9,8 +9,11 @@ import {
   TransitionChild,
 } from "@headlessui/react";
 import { Fragment, useState } from "react";
+import clsx from "clsx";
 export const RecordAddButton = () => {
   const [isOpen, setIsOpen] = useState(false);
+  const [inComeOrExpense, setIncomeOrExpense] = useState("expense");
+  console.log("inComeOrExpense is:", inComeOrExpense);
   const onButtonClick = () => {
     setIsOpen((pre) => !pre);
   };
@@ -54,7 +57,7 @@ export const RecordAddButton = () => {
             <DialogPanel
               className={"fixed inset-0 flex justify-center items-center"}
             >
-              <div className="w-[792px] h-[712px]">
+              <div className="w-[600px] h-[600px] bg-white  rounded-3xl overflow-hidden">
                 <div className="w-full h-[15%] px-6 py-5 flex justify-between items-center border border-b-2">
                   <p
                     className={`${roboto700.className} text-xl text-[#0F172A]`}
@@ -62,15 +65,67 @@ export const RecordAddButton = () => {
                     Add record
                   </p>
                   <XMarkIcon
+                    className="h-5"
                     onClick={() => {
                       close();
                     }}
                   />
                 </div>
+
                 <form className="w-full h-[85%] grid grid-cols-[1fr_1fr]">
-                  <div className="w-full h-full px-6 py-5 flex flex-col">
-                    
+                  <div className="w-full h-full px-6 py-5 flex flex-col gap-5">
+                    <div className="w-full h-fit flex bg-[#F3F4F6] rounded-2xl">
+                      <div
+                        className={clsx(
+                          `${roboto400.className} w-1/2 h-fit py-1 rounded-2xl text-center `,
+                          {
+                            " bg-[#0166FF] text-white":
+                              inComeOrExpense == "expense",
+                          }
+                        )}
+                        onClick={() => {
+                          setIncomeOrExpense("expense");
+                        }}
+                      >
+                        Expense
+                      </div>
+                      <div
+                        className={clsx(
+                          `${roboto400.className} w-1/2 h-fit py-1 rounded-2xl text-center `,
+                          {
+                            "bg-[#16A34A] text-white":
+                              inComeOrExpense == "income",
+                          }
+                        )}
+                        onClick={() => {
+                          setIncomeOrExpense("income");
+                        }}
+                      >
+                        Income
+                      </div>
+                    </div>
+                    <label
+                      htmlFor="amount"
+                      className="h-[76px] p-2 bg-[#F3F4F6] flex flex-col justify-around border border-[#D1D5DB] rounded-lg"
+                    >
+                      <p className={`${roboto400.className} text-[#171717]`}>
+                        Amount
+                      </p>
+                      <input
+                        type="number"
+                        id="amount"
+                        name="amount"
+                        placeholder="₮ 000"
+                        className={`${roboto400.className}  bg-[#F3F4F6] outline-none`}
+                        min={0}
+                      />
+                    </label>
+                    <label htmlFor="category" className="w-full ">
+                      <p className={`${roboto400.className} text-base`}>Category</p>
+                      <div></div>
+                    </label>
                   </div>
+
                   <div className="w-full h-full"></div>
                 </form>
               </div>
